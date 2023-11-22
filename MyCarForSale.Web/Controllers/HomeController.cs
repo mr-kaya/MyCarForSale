f117390c0edbf71c30c7916cf9c7134fca9693ba
Mustafa Kaya <kaya.mustafa.467@gmail.com>
2023-11-22 19:23:16 +0300

    tailwind import Updated

---
+++ MyCarForSale.Web/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MyCarForSale.Web.Models;
+using MyCarForSale.Web.Services;
+
+namespace MyCarForSale.Web.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly CarFeaturesWithImageAndClassificationAndUserAccountService _carFeaturesService;
+    
+    public HomeController(ILogger<HomeController> logger, CarFeaturesWithImageAndClassificationAndUserAccountService carFeaturesService)
+    {
+        _logger = logger;
+        _carFeaturesService = carFeaturesService;
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    
+    public async Task<IActionResult> Index()
+    {
+        var cars = await _carFeaturesService.AllSaleCarsAsync();
+        Random random = new Random();
+        for (int i = 0; i < cars.Count; i++)
+        {
+            int randomIndex = random.Next(cars.Count());
+            (cars[i], cars[randomIndex]) = (cars[randomIndex], cars[i]);
+        }
+        return View(cars);
+    }
+
+    public async Task<IActionResult> Remove(int id)
+    {
+        await _carFeaturesService.RemoveAsync(id);
+        return RedirectToAction(nameof(Index));
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ MyCarForSale.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyCarForSale.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
